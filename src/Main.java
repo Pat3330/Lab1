485a802f85c8c624811cd620c7163e01cac95caf
patco <patcortez163@gmail.com>
2026-01-30 22:59:34 -0500

    this is the lab assignment.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle(4, "Red", 1.5f, "Petrol");
